26952b24d78f131adb002978249a246b84899ebf
acer1_user1 <user1@acer1.com>
2013-08-27 16:02:22 +0800

    add yundao.py

---
+++ src/taobao/dao/yundao.py
@@ -0,0 +1,40 @@
+import datetime
+import MySQLdb
+from taobao.common.utils import getconfig,format_datetime
+
+def get_yunda_conn():
+    
+    conn = MySQLdb.connect(host= "192.168.0.19",
+                  user="root",
+                  passwd="123",
+                  db="ydwd")
+    
+    return conn
+
+def insert_yunda_fjbak(txm,weight):
+    
+    conn = get_yunda_conn()
+    weight = ('.' in weight) and float(weight) or float(weight)/1000.0
+    cfg = getconfig()
+    cr = conn.cursor()
+    cr.execute ("INSERT INTO yjsm_bak(TXM,WPZL,DD,XJDD,YSFS,SJ,SMLB,SMY,SFD) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)", 
+               (txm,
+                str(round(weight,2)),
+                cfg.get('custom','DD'),
+                cfg.get('custom','XJDD'),
+                cfg.get('custom','YSFS'),
+                format_datetime(datetime.datetime.now()),
+                cfg.get('custom','SMLB'),
+                cfg.get('custom','SMY'),
+                cfg.get('custom','SFD')))
+    
+    row = cr.fetchall()
+    return row
+
+#conn = get_yunda_conn()
+#insert_yunda_fjbak('1200907504556','320')
+#cr = conn.cursor()
+#cr.execute('select * from yjsm_bak')
+#print cr.fetchall()
+#
+#conn.close()
