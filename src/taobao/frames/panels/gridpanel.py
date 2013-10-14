5158110b6585a095c9065a2bc09fb659c911e8c1
acer1_user1 <user1@acer1.com>
2013-10-14 19:02:06 +0800

    fix get yunda ids with sort

--- src/taobao/frames/panels/gridpanel.py
+++ src/taobao/frames/panels/gridpanel.py
@@ -460,7 +460,7 @@ class GridPanel(wx.Panel):
     def get_yunda_ids(self):
         
         yunda_ids = []
-        for row in self._selectedRows:
+        for row in list(self._selectedRows).sort():
             
             trade_id = self.grid.GetCellValue(row,TRADE_ID_CELL_COL)
             logistic = self.grid.GetCellValue(row,LOG_COMPANY_CELL_COL)
