51e7f95a11ae883669210c4588bdd5942a537c47
unknown <>
2016-05-11 10:29:18 +0800

    bat

--- huyierp.bat
+++ huyierp.bat
@@ -1,5 +1,4 @@
 if "%1" == "h" goto begin
 mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
 :begin
-;echo  'abc' >> ./test.txt
-python C:\Users\user1\workspace\taobao-erp-client\src\auimain.py
+python .\src\auimain.py
